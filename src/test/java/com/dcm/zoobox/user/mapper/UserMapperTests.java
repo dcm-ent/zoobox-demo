addf3d5580c4d1236c3eeabeb034c1c76bc4ffaf
Taein Kim <rlaxhgus1003@gmail.com>
2021-08-17 13:17:18 +0900

    taein 210817

--- src/test/java/com/dcm/zoobox/user/mapper/UserMapperTests.java
+++ src/test/java/com/dcm/zoobox/user/mapper/UserMapperTests.java
@@ -1,6 +1,7 @@
 package com.dcm.zoobox.user.mapper;
 
 import org.junit.Test;
+
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
--- src/test/java/com/dcm/zoobox/user/service/UserServiceTests.java
+++ src/test/java/com/dcm/zoobox/user/service/UserServiceTests.java
@@ -2,6 +2,7 @@ package com.dcm.zoobox.user.service;
 
 import java.util.Map;
 
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
